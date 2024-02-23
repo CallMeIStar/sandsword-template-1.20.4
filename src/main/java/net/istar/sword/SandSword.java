0e305fcca6516b8c4ab640987a8ced5a72e61738
IStar <stargamemail8@gmail.com>
2024-02-23 23:59:46 +0200

    own

--- src/main/java/net/istar/sword/SandSword.java
+++ src/main/java/net/istar/sword/SandSword.java
@@ -2,6 +2,7 @@ package net.istar.sword;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.istar.sword.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +12,6 @@ public class SandSword implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
