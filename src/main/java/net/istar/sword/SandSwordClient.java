24e68e542a9bb161e2c91f7b074bac38b5422529
unknown <stargamemail8@gmail.com>
2024-02-23 23:43:21 +0200

    Initial commit

---
+++ src/main/java/net/istar/sword/SandSwordClient.java
@@ -0,0 +1,11 @@
+package net.istar.sword;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SandSwordClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
---
+++ src/main/java/net/istar/sword/SandSwordDataGenerator.java
@@ -0,0 +1,11 @@
+package net.istar.sword;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SandSwordDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
