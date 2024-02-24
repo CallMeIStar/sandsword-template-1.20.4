27a3e71615c6431628087d00094f2228828f0469
IStar <stargamemail8@gmail.com>
2024-02-24 17:23:42 +0200

    updated

--- src/main/java/net/istar/sword/SandSwordClient.java
+++ src/main/java/net/istar/sword/SandSwordClient.java
@@ -1,16 +1,10 @@
 package net.istar.sword;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.istar.sword.client.renderer.SandSwordRenderer;
-import net.istar.sword.entity.ModEntities;
 
 public class SandSwordClient implements ClientModInitializer
 {
     @Override
     public void onInitializeClient(){
-        EntityRendererRegistry.register(ModEntities.DUNEEDGE, (dispatcher, context) -> {
-            return new SandSwordRenderer(dispatcher);
-        });
     }
 }
--- src/main/java/net/istar/sword/entity/ModEntities.java
+++ src/main/java/net/istar/sword/entity/ModEntities.java
@@ -1,5 +1,6 @@
 package net.istar.sword.entity;
 
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.istar.sword.SandSword;
 import net.istar.sword.entity.custom.SandSwordEntity;
@@ -10,7 +11,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
-public class ModEntities {
+public abstract class ModEntities implements ModInitializer {
     public static final EntityType<SandSwordEntity> DUNEEDGE = Registry.register(Registries.ENTITY_TYPE,
             new Identifier(SandSword.MOD_ID, "duneedge"), FabricEntityTypeBuilder.<SandSwordEntity>create(SpawnGroup.MISC, SandSwordEntity:: new)
                     .dimensions(EntityDimensions.fixed(0.5f,0.5f)).build());
